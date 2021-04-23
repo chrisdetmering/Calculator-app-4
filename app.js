4440bf9312d4e9af43aed6176727fd7b1cfb154e
dmalchowLFC <80545257+dmalchowLFC@users.noreply.github.com>
2021-04-22 21:03:52 -0700

    Updated status
    buttons all working as expected, now working to get the operators working

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const screen = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".nums");
+const operators = document.querySelectorAll(".operators");
+const total = 0;
+const a = screen.value;
 
-// document.getElementsByClassName("nums").addEventListener("click", append);
+numberButtons.forEach(numberButton => {
+    numberButton.addEventListener('click', () => {
+        screen.value += (numberButton.textContent);
+    })
+})
 
-document.getElementById("one").addEventListener("click", append1);
-document.getElementById("two").addEventListener("click", append2);
-document.getElementById("three").addEventListener("click", append3);
-document.getElementById("four").addEventListener("click", append4);
-document.getElementById("five").addEventListener("click", append5);
-document.getElementById("six").addEventListener("click", append6);
-document.getElementById("seven").addEventListener("click", append7);
-document.getElementById("eight").addEventListener("click", append8);
-document.getElementById("nine").addEventListener("click", append9);
 
 document.getElementById("divide").addEventListener("click", division);
 document.getElementById("multiply").addEventListener("click", multiplication);
@@ -18,62 +17,10 @@ document.getElementById("subtract").addEventListener("click", subtraction);
 document.getElementById("add").addEventListener("click", addition);
 document.getElementById("equal").addEventListener("click", solve);
 
-// function append(e) {
-//     e.preventDefault()
-//     if (e.target.getElementById === "one") {
-//         screen.value += 1;
-//     } else if (e.target.getElementById.contains('two')) {
-//         screen.value += 2;
-//     } else if (e.target.getElementById.contains('three')) {
-//         screen.value += 3;
-//     } else if (e.target.getElementById.contains('four')) {
-//         screen.value += 4;
-//     } else if (e.target.getElementById.contains('five')) {
-//         screen.value += 5;
-//     } else if (e.target.getElementById.contains('six')) {
-//         screen.value += 6;
-//     } else if (e.target.getElementById.contains('seven')) {
-//         screen.value += 7;
-//     } else if (e.target.getElementById.contains('eight')) {
-//         screen.value += 8;
-//     } else if (e.target.getElementById.contains('nine')) {
-//         screen.value += 9;
-//     }
-// };
+function addition(e) {
+    e.preventDefault();
+    total += a;
+    a = total;
+};
 
-function append1(e) {
-    e.preventDefault()
-    screen.value += 1;
-}
-function append2(e) {
-    e.preventDefault()
-    screen.value += 2;
-};
-function append3(e) {
-    e.preventDefault()
-    screen.value += 3;
-};
-function append4(e) {
-    e.preventDefault()
-    screen.value += 4;
-};
-function append5(e) {
-    e.preventDefault()
-    screen.value += 5;
-};
-function append6(e) {
-    e.preventDefault()
-    screen.value += 6;
-};
-function append7(e) {
-    e.preventDefault()
-    screen.value += 7;
-};
-function append8(e) {
-    e.preventDefault()
-    screen.value += 8;
-};
-function append9(e) {
-    e.preventDefault()
-    screen.value += 9;
-};
+
