220bccc4b55d2fcb9b1e0b37ecd0b1a52830380d
dmalchowLFC <80545257+dmalchowLFC@users.noreply.github.com>
2021-04-23 22:42:26 -0700

    Almost complete
    I've made it so that the buttons print to the screen, the two variables are defined and the calculator can now perform 1 mathematical operation. I still want to update it further so that I can perform more math in continuation.

--- app.js
+++ app.js
@@ -1,26 +1,115 @@
 const screen = document.getElementById("display");
+const equation = document.getElementById("display2");
+let operate = '';
+let firstNumber = '';
+let secondNumber = '';
+let resolved = 0;
+
+
+
 const numberButtons = document.querySelectorAll(".nums");
-const operators = document.querySelectorAll(".operators");
-const total = 0;
-const a = screen.value;
-
 numberButtons.forEach(numberButton => {
-    numberButton.addEventListener('click', () => {
-        screen.value += (numberButton.textContent);
+    numberButton.addEventListener('click', (e) => {
+        const number = e.target.textContent;
+        screen.value += number;
+        equation.value += number;
+
+        if (operate === '') {
+            firstNumber += number;
+        }
+        if (operate != '') {
+            if (secondNumber === '') {
+                screen.value = '';
+                screen.value += number;
+            }
+            secondNumber += number;
+        }
+
+
+        console.log('first number', firstNumber);
+        console.log("second number", secondNumber);
+        console.log("operator", operate);
     })
-})
+});
+
+const operators = document.querySelectorAll(".operators");
+operators.forEach(operator => {
+    operator.addEventListener("click", (e) => {
+        const selectedOperator = e.target.textContent;
+
+        if (firstNumber != '' && operate === '') {
+            operate = selectedOperator;
+            screen.value += ' ' + operate;
+            equation.value += ' ' + operate + ' ';
+            console.log(operate);
+        };
+
+        if (secondNumber != '') {
+
+        }
+    })
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+    if (secondNumber === '') {
+        firstNumber = '';
+        operate = '';
+        screen.value = '';
+        equation.value = '';
+    }
+
+    if (secondNumber != '' && screen.value != '') {
+        screen.value = '';
+        secondNumber = '';
+        equation.value = firstNumber + ' ' + operate + ' ';
+    }
+});
 
 
-document.getElementById("divide").addEventListener("click", division);
-document.getElementById("multiply").addEventListener("click", multiplication);
-document.getElementById("subtract").addEventListener("click", subtraction);
-document.getElementById("add").addEventListener("click", addition);
-document.getElementById("equal").addEventListener("click", solve);
+const equals = document.querySelector("#equal");
+equals.addEventListener("click", () => {
+    if (secondNumber === '') {
+        return
+    };
+    const regex = /[0-9+][.][0-9+]/gm;
+    let a = '';
+    let b = '';
 
-function addition(e) {
-    e.preventDefault();
-    total += a;
-    a = total;
-};
+    if (regex.test(firstNumber) === true) {
+        a = parseFloat(firstNumber);
+    } else {
+        a = parseInt(firstNumber);
+    }
 
 
+    if (regex.test(firstNumber) === true) {
+        b = parseFloat(secondNumber);
+    } else {
+        b = parseInt(secondNumber);
+    }
+
+
+    if (operate === "+") {
+        let solve = a + b;
+        screen.value = solve;
+        equation.value += " = " + solve;
+        console.log(solve);
+    } else if (operate === "-") {
+        let solve = a - b;
+        screen.value = solve;
+        equation.value += " = " + solve;
+        console.log(solve);
+    } else if (operate === "x") {
+        let solve = a * b;
+        screen.value = solve;
+        equation.value += " = " + solve;
+        console.log(solve);
+    } else if (operate === "÷") {
+        let solve = a / b;
+        screen.value = solve;
+        equation.value += " = " + solve;
+        console.log(solve);
+    };
+
+});
